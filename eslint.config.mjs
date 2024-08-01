9b8487f732f4a2ede62bb7f7e599d44ab012d5c9
samuelhulla <oficialny.hulla@gmail.com>
2024-08-01 18:32:16 +0200

    :wrench: update: (repo) disable unnecessary rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,15 @@ import prettier from "eslint-plugin-prettier/recommended"
 
 export default tseslint.config(eslint.configs.recommended, prettier, ...tseslint.configs.recommended, {
   ignores: ["**/node_modules", "**/dist"],
+  rules: {
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        types: {
+          "{}": false,
+        },
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+  },
 })
