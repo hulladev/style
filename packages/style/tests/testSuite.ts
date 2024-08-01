d8fe255b89bc01a0e21f07024d429ec9a97324c3
samuelhulla <oficialny.hulla@gmail.com>
2024-08-01 18:31:22 +0200

    :test_tube: tests: (package) update tests for modifiers + multi props syntax

--- packages/style/tests/testSuite.ts
+++ packages/style/tests/testSuite.ts
@@ -1,21 +1,20 @@
 import { expect, test } from "vitest"
-import type { style } from "../src"
-import type { Compose } from "../src/types"
+import type { Compose, VariantFunction } from "../src/types"
 
 type Suites = "vanilla" | "clsx" | "twJoin" | "twMerge"
 
 // this test suite is re-run multiple times based on the passed cn, to make it DRY
-export const testSuite = <CN extends Compose, VN extends ReturnType<typeof style<CN>>["vn"]>(
-  suite: Suites,
-  cn: CN,
-  vn: VN
-) => {
+export const testSuite = <CN extends Compose, VN extends VariantFunction>(suite: Suites, cn: CN, vn: VN) => {
   const variant = vn({
-    variants: {
-      default: "hello",
-      world: "world",
+    props: {
+      variant: {
+        default: "hello",
+        world: "world",
+      },
     },
-    defaultVariant: "default",
+    defaults: {
+      variant: "default",
+    },
   })
   test("exports", () => {
     expect(vn).toBeDefined()
@@ -34,14 +33,34 @@ export const testSuite = <CN extends Compose, VN extends ReturnType<typeof style
   })
   test("base styles work", () => {
     const baseVariant = vn({
-      variants: {
-        world: "world",
+      props: {
+        variant: {
+          world: "world",
+          me: "me",
+        },
       },
-      defaultVariant: "world",
+      defaults: {
+        variant: "world",
+      },
       base: "base",
     })
     expect(baseVariant({ variant: "world" })).toStrictEqual("base world")
+    expect(baseVariant({ variant: "me" })).toStrictEqual("base me")
     expect(baseVariant({})).toStrictEqual("base world")
+  })
+  test("without specifying default variants", () => {
+    const variant = vn({
+      base: "base",
+      props: {
+        variant: {
+          world: "world",
+          me: "me",
+        },
+      },
+    })
+    expect(variant({ variant: "world" })).toStrictEqual("base world")
+    expect(variant({ variant: "me" })).toStrictEqual("base me")
+    expect(variant({})).toStrictEqual("base")
   })
   test("extra custom props", () => {
     const value = variant({ class: "class", className: "className", "class:list": ["class", "list"] })
@@ -53,10 +72,71 @@ export const testSuite = <CN extends Compose, VN extends ReturnType<typeof style
   })
   test("duplicate prop", () => {
     const value = variant({ class: "mx-0 mx-0" })
+    // only my default compose implementation and tailwing-merge work
     if (suite === "twMerge" || suite === "vanilla") {
       expect(value).toStrictEqual("hello mx-0")
     } else {
       expect(value).toStrictEqual("hello mx-0 mx-0")
     }
+  })
+  test("multiple props", () => {
+    const value = vn({
+      base: "base",
+      props: {
+        variant: {
+          world: "world",
+          me: "me",
+        },
+        size: {
+          sm: "sm",
+          md: "md",
+        },
+      },
+      defaults: {
+        variant: "world",
+        size: "sm",
+      },
+    })
+    expect(value({})).toStrictEqual("base world sm")
+    expect(value({ variant: "me" })).toStrictEqual("base me sm")
+    expect(value({ size: "md" })).toStrictEqual("base world md")
+    expect(value({ size: "md", variant: "me" })).toStrictEqual("base me md")
+    expect(value({ size: "md", variant: "world" })).toStrictEqual("base world md")
+  })
+  test("running with modifier", () => {
+    const withMod = vn(
+      {
+        props: {
+          variant: {
+            world: "world",
+            me: "me",
+          },
+        },
+      },
+      ({ props }) => (props.variant === "world" ? { base: "base" } : {})
+    )
+    expect(withMod({ variant: "world" })).toStrictEqual("base world")
+    expect(withMod({ variant: "me" })).toStrictEqual("me")
+  })
+  test("props modifier", () => {
+    const vnOverride = vn as unknown as VariantFunction<{ foo: boolean }>
+    const vnp = vnOverride(
+      {
+        props: {
+          variant: {
+            world: "world",
+            me: "me",
+          },
+        },
+      },
+      ({ props }) => {
+        if (props.foo) {
+          return { base: "base" }
+        }
+        return {}
+      }
+    )
+    expect(vnp({ variant: "world", foo: true })).toStrictEqual("base world")
+    expect(vnp({ variant: "world", foo: false })).toStrictEqual("world")
   })
 }
